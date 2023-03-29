3abcca0f3d556cde6a14d956202d3b7c93e45b21
Илья <ilabelousov123593@gmail.com>
2023-03-29 19:13:39 +0300

    4

--- main.py
+++ main.py
@@ -33,5 +33,25 @@ def third_num():
     </body></html>"""
 
 
+@app.route('/promotion_image')
+def fourth_num():
+    return f"""<html>
+    <head>
+        <tile>Привет, Марс!</tile>
+        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+            integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    </head>
+    <body>
+    <h1>Жди нас, Марс!</h1>
+    <img src="{url_for('static', filename='img/MARS.png')}" alt="здесь должна была быть картинка, но не нашлась">
+    <p class="alert alert-secondary" role="alert">Человечество вырастает из детства.</p>
+    <p class="alert alert-primary" role="alert">Человечеству мала одна планета.</p>
+    <p class="alert alert-warning" role="alert"> Мы сделаем обитаемыми безжизненные пока планеты.</p>
+    <p class="alert alert-success" role="alert">И начнем с Марса!</p>
+    <p class="alert alert-danger" role="alert">Присоединяйся!</p>
+    </body></html>"""
+
+
 if __name__ == "__main__":
     app.run(port=8080, host='127.0.0.1')
